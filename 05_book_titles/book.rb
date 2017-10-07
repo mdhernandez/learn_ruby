4a71fd7737573e01ab9325cff962977bcd2974ee
mdhernandez <md.hernandez1992@hotmail.com>
2017-10-07 04:16:32 -0400

    Book Class Written

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+  attr_reader :title
+
+  def title = (str)
+	words = str.split(' ')
+	word_len = words.length
+	i = 0
+	little = %w{a an and the in of}
+	while i < word_len
+	  if !(little.include? words[i])
+	    words[i].capitalize!
+	  end#end if
+	end#end while
+	@title = words.join(' ')
+  end#end writer
 end
